597d3a7cb3a1492650c277a4e3ac24ddf55e4955
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-03-15 12:01:14 +0100

    about section update

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <!-- <li class="nav-item active">
+                        <a class="nav-link" href="#section-home">Home <span class="sr-only">(current)</span></a>
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#section-about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#section-community">Community</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#section-portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#section-hackaton">Hackathon</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#section-blog">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#section-contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,228 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <nav-bar></nav-bar>
+
+      <Section id="section-home">
+        <div class="container">
+          <h2>Hello</h2>
+          <p>
+            I am Daniel Nonso Chukwurah, A Front-end Engineer based in Lagos, Nigeria. I help brands and organistions
+            build scalable web applications and softwares that are user friendly, accesible, usuable and achieve their
+            goals using the latest technologies.
+          </p>
+          <button>Get in Touch</button>
+
+          <!-- <p>
+            I'm passionate about building softwares with good user experience, good designs, accessible, test driven and
+            writing clean codes to the best of my ability
+          </p> -->
+        </div>
+
+      </Section>
+
+      <section id="section-about">
+        <p>
+          I'm currently a Front-end Engineer at Sidmach Technologies Limited building enterprise web solutions and
+          softwares together with amazing teams for amazing clients. I am a graduate of Agriculture from the University
+          of Benin. I also graduated from the Microsoft AppFactory programme after completing top notch projects.
+
+          As a Front-end Engineer, I'm passionate about building softwares with good user experience, good designs,
+          that's accessible, test driven writing reus
+          writing clean codes to the best of my ability. I also enjoy learning new technologies.
+
+          I enjoy bridging the gap between engineering and design - combining state-of-the-art
+          technical knowledge with a keen eye for design to create a most beautiful product and services. My goal is to
+          always build applications that are scalable and efficient under the hood while providing engaging user
+          experience.
+
+
+          When I'm not coding you can find me surfing the internet, playing and watching football, playing video games
+          (FIFA, PES), or hanging out with friends/family.
+        </p>
+
+        My name is Nenne Adaora Nwodo. I fondly like to be called Adora. I am a Software Engineer based in Lagos,
+        Nigeria who loves mobile experiences and artificial intelligence.
+
+        I currently work as a Software Engineer at the Microsoft Cognition team & I have a Bachelor of Science (First
+        class) in Computer Science from the University of Lagos, Nigeria.
+
+        In October 2018, I created a blog called AdoraHack, where I post software development articles or any tech thing
+        I find interesting. In June 2019, I took AdoraHack to YouTube. On the YouTube channel, I post tutorials and
+        other tech content that could be useful to software developers.
+
+        My non-tech passion includes education & music. I believe everyone should be given a chance at basic education.
+
+        In tech, I am a huge fan of great mobile experiences driven by AI. At a tech event, you'd probably find me in a
+        booth with a VR headset on, or trying to play a game (and possibly failing at it).
+
+        I am also passionate about the developer community and I am trying to drive inclusion for more women in tech.
+        Because of this, I co-organize some developer events (e.g. GDG Ajah, FSD Lagos, Android Nigeria), I speak at
+        some events as well & I mentor developers through ALC or any other platform that lets me do so.
+
+      </section>
+
+      <section class="site-section" id="section-about">
+        <div class="container">
+          <div class="row mb-5 align-items-center">
+            <div class="col-lg-7 pr-lg-5 mb-5 mb-lg-0 pt-5 pb-5">
+              <img class="img-fluid" id="about-me-pics" src="images\Daniel_Chukwurah.JPG" alt="Image placeholder">
+            </div>
+            <div class="col-lg-5 pl-lg-5">
+              <div class="section-heading">
+                <h2>About <strong>Me</strong></h2>
+              </div>
+              <p class="lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
+                language
+                ocean.</p>
+              <p class="mb-5  ">A small river named Duden flows by their place and supplies it with the necessary
+                regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+
+              <p>
+                <a class="btn btn-primary px-4 py-2 btn-sm smoothscroll" href="#section-contact">Continue</a>
+                <!-- <a class="btn btn-secondary px-4 py-2 btn-sm" href="#">Certifications</a> -->
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-skills">
+        <p>
+          I am proficient in quite some skills and on my way to becoming a professional. Here are a few of the
+          technologies I have experience with:
+        </p>
+      </section>
+
+      <section class="site-section" id="section-blog">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 mb-5">
+              <div class="section-heading text-center">
+                <h2>Blog on <strong>Medium</strong></h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-sm-6 col-lg-4 mb-4">
+              <div class="blog-entry">
+                <a href="#"><img class="img-fluid" src="images/post_1.jpg" alt="Image placeholder"></a>
+                <div class="blog-entry-text">
+                  <h3><a href="#">Creative Product Designer From Facebook</a></h3>
+                  <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost
+                    unorthographic.</p>
+
+                  <div class="meta">
+                    <a href="#"><span class="icon-calendar"></span> Aug 7, 2018</a>
+                    <a href="#"><span class="icon-bubble"></span> 5 Comments</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 mb-4">
+              <div class="blog-entry">
+                <a href="#"><img class="img-fluid" src="images/post_2.jpg" alt="Image placeholder"></a>
+                <div class="blog-entry-text">
+                  <h3><a href="#">Creative Product Designer From Facebook</a></h3>
+                  <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost
+                    unorthographic.</p>
+
+                  <div class="meta">
+                    <a href="#"><span class="icon-calendar"></span> Aug 7, 2018</a>
+                    <a href="#"><span class="icon-bubble"></span> 5 Comments</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 mb-4">
+              <div class="blog-entry">
+                <a href="#"><img class="img-fluid" src="images/post_3.jpg" alt="Image placeholder"></a>
+                <div class="blog-entry-text">
+                  <h3><a href="#">Creative Product Designer From Facebook</a></h3>
+                  <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost
+                    unorthographic.</p>
+
+                  <div class="meta">
+                    <a href="#"><span class="icon-calendar"></span> Aug 7, 2018</a>
+                    <a href="#"><span class="icon-bubble"></span> 5 Comments</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section class="site-section bg-light " id="section-community">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 mb-5">
+              <div class="section-heading text-center">
+                <h2> <strong>Community</strong></h2>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <h2 class="mb-5">Lepsta Developers</h2>
+              <div class="resume-item mb-4">
+                <span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+                <h3>Masteral in Information Technology</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+                </p>
+                <span class="school">New York University</span>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <h2 class="mb-5">GDG Ajah</h2>
+              <div class="resume-item mb-4">
+                <span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+                <h3>Masteral in Information Technology</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+                </p>
+                <span class="school">New York University</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="site-section bg-light" id="section-contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 mb-5">
+              <div class="section-heading text-center">
+                <h2>Get in <strong>Touch </strong> With Me?</h2>
+              </div>
+            </div>
+
+            <div class="col-md-12 mb-5 mb-md-0 text-center">
+              <p>I am opened to opportunities, collaborations, speaking engagement, developers community work,
+                volunteering and relocation. I am mostly available on <a href="">email</a>, <a href="">twitter</a> and
+                <a href="">LinkedIn</a> . You
+                can
+                send me a message and I’ll reply as soon as possible.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </main>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import NavBar from '@/components/NavBar.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'navbar',
+    components: {
+      'nav-bar': NavBar,
+    }
   }
-}
 </script>

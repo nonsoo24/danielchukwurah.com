5de0655872b6fb5eaf327182bc9e058953c1ee14
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-09-19 07:05:25 +0100

    feat: handle error

--- src/main.js
+++ src/main.js
@@ -7,6 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
+window.onerror = function(message, source, line, column, error) {
+  console.log('ONE ERROR HANDLER TO RULE THEM ALL:', message);
+}
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+Vue.config.errorHandler = function(err, vm, info) {
+  console.log(`Error: ${err.toString()}\nInfo: ${info}`);
+}
+Vue.config.warnHandler = function(msg, vm, trace) {
+  console.log(`Warn: ${msg}\nTrace: ${trace}`);
+}
+
 export const eventBus = new Vue({
   methods: {
     closeBtn(){
